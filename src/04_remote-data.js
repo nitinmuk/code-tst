5090e8d16dc68a353c58fa77bd49b543e112d950
Nitin Mukesh <nitin.mukesh@sine.co>
2022-02-19 07:59:38 +1030

    Implemented solution function

--- src/04_remote-data.js
+++ src/04_remote-data.js
@@ -1,3 +1,7 @@
+const got = require('got');
+
+const todosURL = 'https://jsonplaceholder.typicode.com/todos';
+const usersURL = 'https://jsonplaceholder.typicode.com/users';
 /**
  *  This function fetches all todos from https://jsonplaceholder.typicode.com/todos
  *  and all users from https://jsonplaceholder.typicode.com/users
@@ -6,8 +10,15 @@
  *  and todos = [{ id: 2, userId: 1, completed: true}]
  *  Then the function would output [{ username: 'Tom', completed: 0}, { username: 'Jerry', completed: 1}]
  */
-function solution () {
-
+async function solution () {
+    const todosPromise = got(todosURL).json();
+    const usersPromise = got(usersURL).json();
+    const [users, todos] = await Promise.all([usersPromise, todosPromise]);
+    return users?.map(user => {
+        const completed = todos?.filter(todo => todo.userId === user.id && todo.completed).length;
+        const { username } = user;
+        return { username, completed };
+    });
 }
 
 module.exports = solution
