c8b806581a0c66a943bcfc8ed6a95b22a05c3ed9
Nitin Mukesh <nitin.mukesh@sine.co>
2022-02-19 08:13:59 +1030

    implemented solution for converting object to array

--- src/01_convert-object-to-array.js
+++ src/01_convert-object-to-array.js
@@ -2,8 +2,6 @@
  * This function returns the values of the input object as an array
  * @param {object} obj
  */
-function solution (obj) {
-
-}
+const solution = obj => Object.values(obj);
 
 module.exports = solution
